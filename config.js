f5a3d8474cafccff4e0d2a4410cdf4a1217cda71
Ubuntu <ubuntu@ip-172-31-17-68.us-west-2.compute.internal>
2017-10-09 15:46:53 +0000

    Working on base REST API

--- config.js
+++ config.js
@@ -1,7 +1,2 @@
 var portNo = process.env.PORT || 3000;
 module.exports.port = portNo;
-
-{
-  "GCLOUD_PROJECT": "cs-496-180914",
-  "DATA_BACKEND": "datastore"
-}
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+// Get Frameworks
 const express = require('express');
-const server = express();
 const config = require('./config.js');
+const Datastore = require('@google-cloud/datastore');
+const crypto = require('crypto');
+
+
+// Initialize Objects
+const server = express();
+const datastore = Datastore();
 
 server.set('view engine', 'ejs');
 
@@ -8,16 +15,10 @@ var socketServer = server.listen(config.port, () => {
   console.log('Express is listening on port ' + config.port);
 });
 
-// Boats
-server.use('/boats', require('./boats/crud'));
-server.use('/api/books', require('./boats/api'));
-
-// Redirect to Boats
-server.get('/', (req, res) => {
-	res.redirect('/boats');
-});
+//REST API - Boats
 
 /*
+// Temporary Landing Page
 server.get('/', (req, res) => {
 	res.render('pages/index', {
 		serverVar: "Variable from Server"
@@ -25,8 +26,58 @@ server.get('/', (req, res) => {
 });
 */
 
+/** 
+  * Insert a 'boat' record into the database
+  * 
+  * @param {boat} boat - The boat record to insert
+  */
+function insertRec(boat){
+	return datastore.save({
+		key: datastore.key('boat'),
+		data: boat
+	});
+}
+
+/** 
+  * Retrieve the latest 10 boat records from the database
+  */
+function getBoats(){
+	const query = datastore.createQuery('boat')
+		.order('timestamp', {descending: true })
+		.limit(10);
+
+	return datastore.runQuery(query) 
+		.then((results) => {
+			const entities = results[0];
+			return entities.map((entity) => 'Time: ${entity.timestamp}, AddrHash: ${entity.userIp}');
+		});
+}
+
+// REST API - Insert Record Route
+server.get('/', (req, res, next) => {
+	// Create a boat record to be stored in the database
+	const boat = {
+		timestamp: new Date(),
+		// Store a hash of the IP address of the user doing the insertion
+		userIp: crypto.createHash('sha256').update(req.ip).digest('hex').substr(0, 7)
+	};
+
+	insertRec(boat)
+		.then(() => getBoats())
+		.then((boats) => {
+			res
+				.status(200)
+				.set('Content-Type', 'text/plain')
+				.send(`Last 10 boats:\n${boats.join('\n')}`)
+				.end();
+		})
+		.catch(next);
+});
+
+// 404 Error Handler
 server.use((req, res) => {
 	res.status(404).send('Not Found');
 });
 
 server.use(express.static('public'));
+module.exports = server;
