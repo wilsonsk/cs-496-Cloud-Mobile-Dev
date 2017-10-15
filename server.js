ba27e758ab8497668a6d3bc21caed318876910be
Ubuntu <ubuntu@ip-172-31-17-68.us-west-2.compute.internal>
2017-10-15 06:59:06 +0000

    REST API for boat entities boilerplate setup

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const express = require('express');
 const config = require('./config.js');
 const Datastore = require('@google-cloud/datastore');
 const crypto = require('crypto');
-
+const bodyParser = require('body-parser');
 
 // Initialize Objects
 const server = express();
 const datastore = Datastore();
+
+//Body Parser
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
 
 server.set('view engine', 'ejs');
 
@@ -62,28 +66,18 @@ function retrieveBoat(boatId){
 		return datastore.runQuery(query) 
 			.then((results) => {
 				const entities = results[0];
-				console.log(`TEST!: ${entities}`);
-				return entities.map((entity) => `Entity.timestamp: ${entity.timestamp}, Entity key: ${entity[datastore.KEY].id}`);
-				//return entities.map((entity) => `Entity.timestamp: ${entity.timestamp}, Entity key: ${Key('boat', entity[datastore.KEY].id})`);
-				//return entities.map((entity) => `Entity.timestamp: ${entity.timestamp}, Entity key: ${datastore.key('boat', entity[datastore.KEY].id})`);
+				//return entities.map((entity) => `Entity.timestamp: ${entity.timestamp}, Entity id: ${entity[datastore.KEY].id}`);
+				return entities.map((entity) => `Entity id: ${entity[datastore.KEY].id}, Entity.name: ${entity.name}, Entity.type: ${entity.type}, Entity.length: ${entity.length}, Entity.at_sea: ${entity.at_sea}`);
 			});
 	}else{
 		const query = datastore.createQuery('boat')
 			.filter('__key__', '=', boatId);
-			/*.filter('__key__', '=', datastore.key([
-				'boat',
-				boatId
-			]));*/
 
 		return datastore.runQuery(query)
 			.then((result) => {
 				const entities = result[0];
-				//return entity.map((e) => `Entity.timestamp: ${e.timestamp}, Entity key: ${e[datastore.KEY].id}`);
-				//return `Entity.timestamp: ${entity.timestamp}, Entity key: ${entity[datastore.KEY].id}`;
 				const entity = entities[0];
 				var key = entity[datastore.KEY];
-				console.log(`NEwTEST: ${JSON.stringify(key)}`);
-				//return entity;
 				return key;
 			});
 	}
@@ -111,18 +105,13 @@ server.get('/', (req, res, next) => {
   * 			- responds with a string containing the boat id returned from the datastore entity
   */
 server.get('/:boatId', (req, res, next) => {
-	//res.send(`boatId = ${req.params.boatId}`);
-	var key = datastore.key(['boat', parseInt(req.params.boatId)]);
-	console.log(key);
+	var key = datastore.key(['boat', parseInt(req.params.boatId)]); // req.params.<> gets parameters from URL
 	retrieveBoat(key)
 		.then((boat) => {
 			res
 				.status(200)
 				.set('Content-Type', 'text/plain')
-				//.send(`Boat ${req.params.boatId}: \n${boat}`)
-				//.send(`Boat ${boat[datastore.KEY]}:\n${boat.timestamp}`)
-				//.send(`boatId = ${req.params.boatId}`);
-				.send(`Boat: ${key}`);
+				.send(`Boat: ${JSON.stringify(boat)}`);
 		})
 		.catch(next);
 });
@@ -132,14 +121,17 @@ server.get('/:boatId', (req, res, next) => {
   * POST Record Route - Calls createBoat() which initializes a new boat entity with appropriate auto and user defined properties.
   * 			- response redirects to '/'
   */
-server.post('/newBoat', (req, res, next) => {
+server.post('/boats', (req, res, next) => {
 	// Create a boat record to be stored in the database
 	const boat = {
+		name: req.body.name, //req.body.<> gets parameters from body of request
+		type: req.body.boatType,
+		length: req.body.boatLength,
+		at_sea: true,
 		timestamp: new Date()
 		// Store a hash of the IP address of the user doing the insertion
 		//userIp: crypto.createHash('sha256').update(req.ip).digest('hex').substr(0, 7)
 	};
-
 	createBoat(boat)
 		.then(res.redirect('/'));
 });
