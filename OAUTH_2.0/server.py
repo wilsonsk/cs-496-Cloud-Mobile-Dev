fc56d5acd76404d5cc3991033a262b741c839403
Ubuntu <ubuntu@ip-172-31-17-68.us-west-2.compute.internal>
2017-10-20 22:26:15 +0000

    Completed OAUTH2.0 implementation without the use of any third party OAUTH libraries

--- OAUTH_2.0/server.py
+++ OAUTH_2.0/server.py
@@ -10,9 +10,9 @@ JINJA_ENV = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates")
 )
 
-# Client app's id, state (ie secret)
-CLIENT_SECRET = "505248232602-593ge8k5au4vleot8g1l11bs6k9iq5tm.apps.googleusercontent.com"
-CLIENT_ID = "_n54-cCQxg2AjLJjV68mInIA"
+# Google OAUTH2.0 API Client app's id, state (ie secret)
+CLIENT_SECRET = "_n54-cCQxg2AjLJjV68mInIA"
+CLIENT_ID = "505248232602-593ge8k5au4vleot8g1l11bs6k9iq5tm.apps.googleusercontent.com"
 
 # Apps main URL
 APP_URL = "https://oauth-183523.appspot.com"
@@ -25,6 +25,9 @@ GOOGLE_OAUTH_REQ_URL = "https://accounts.google.com/o/oauth2/v2/auth"
 
 # URL where the Client uses token to access End User's data
 GOOGLE_PLUS_URL = "https://www.googleapis.com/plus/v1/people/me"
+
+# OAUTH2.0 initial "Client" secret
+init_secret = "SuperSecret9000"
 
 #
 # ROUTE HANDLERS
@@ -44,7 +47,7 @@ GOOGLE_PLUS_URL = "https://www.googleapis.com/plus/v1/people/me"
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		# Build OAUTH GET request URL
-		end_user_server_req = GOOGLE_OAUTH_REQ_URL + "?" + 'response_type=code&' + 'client_id=' + CLIENT_ID + "&" + 'redirect_uri=' + END_USER_REDIRECT_URL + "&" + 'scope=email&' + 'state=SuperSecret9000'
+		end_user_server_req = GOOGLE_OAUTH_REQ_URL + "?" + 'response_type=code&' + 'client_id=' + CLIENT_ID + "&" + 'redirect_uri=' + END_USER_REDIRECT_URL + "&" + 'scope=email&state=' + init_secret
 		
 		# Send this URL request string to the jinja2 template and insert value into a UI button's href field
 		context = {
@@ -52,7 +55,7 @@ class MainHandler(webapp2.RequestHandler):
 			"end_user_request": end_user_server_req 
 		}
 		template = JINJA_ENV.get_template('index.html')
-		self.response.out.write(template.render(context))
+		self.response.write(template.render(context))
 
 # OAUTH Handler
 # 		- "Client" handles the redirected "End User" coming from the Google OAUTH2.0 
@@ -74,11 +77,74 @@ class MainHandler(webapp2.RequestHandler):
 #				- uses token (as header value) to get access to "End User" protected resources
 #					- 'Authoriation': 'Bearer ' + token['access_token']
 #			- "Client" uses jinja2 template to display out the protected resources
+class OauthHandler(webapp2.RequestHandler):
+	def get(self):
+		# Extract state and authorization code from "End User"
+		req_state = self.request.get('state')
+		req_auth_code = self.request.get('code')
+		# verify state
+		if req_state != init_secret:
+			context = {
+				"error": "Initial state does not match the End User's (browser's) state property"
+			}
+			template = JINJA_ENV.get_template('500.html')
+			self.response.write(template.render(context))
+			return
+		
+		# Build OAUTH2.0 token request header and body params
+		header = {
+			"Content-Type": "application/x-www-form-urlencoded"
+		}
+
+		body = {
+			"code": req_auth_code,
+			"client_id": CLIENT_ID,
+			"client_secret": CLIENT_SECRET,
+			"redirect_uri": END_USER_REDIRECT_URL,
+			"grant_type": "authorization_code"
+		}
+
+		# URL encode body params
+		url_encoded_body = urllib.urlencode(body)
+
+		# Make POST request to OAUTH2.0 to obtain token
+		res = urlfetch.fetch("https://www.googleapis.com/oauth2/v4/token/", headers=header, payload=url_encoded_body, method=urlfetch.POST)
+		
+		# Convert token string into JSON object - res.content contains the object that has the token properties
+		token = json.loads(res.content)
+		
+		# Build Google Plus request header, and body params -- requires https
+		header = {
+			"Authorization": "Bearer " + token["access_token"]
+		}
+
+		res = urlfetch.fetch(GOOGLE_PLUS_URL, headers=header)
+		
+		# Convert Google Plus response into JSON object - res.content contains the object that has the Google Plus Resricted Properties
+		url_encoded_gplus_res_data = json.loads(res.content)
+		
+		template = JINJA_ENV.get_template('gplus_results.html')
+		# Extract Google Plus Restricted Properties from url encoded response data from GPlus and add to template for display 
+		context = {
+			'user_name': url_encoded_gplus_res_data['displayName'],
+			'email': url_encoded_gplus_res_data['emails'][0]['value']
+		}
+
+		# write response
+		self.response.write(template.render(context))
 
 #
 # NotFoundHandler()
 #
 #
+class ErrorHandler(webapp2.RequestHandler):
+	def get(self):
+		context = {
+			"title": "Page Not Found",
+			"error": "500 Resource Not Found"	
+		}
+		template = JINJA_ENV.get_template("500.html")
+		self.response.write(template.render(context))
 
 # OPTIONAL PATCH METHOD ENABLER
 allowed_methods = webapp2.WSGIApplication.allowed_methods
@@ -86,7 +152,7 @@ new_allowed_methods = allowed_methods.union(('PATCH',))
 webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 
 # Set http routes.
-app = webapp2.WSGIApplication([('/', MainHandler)],
-#                               ('/oauth', OauthHandler),
-#                               ('/.*', NotFoundHandler)],
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/oauth', OauthHandler),
+                               ('/.*', ErrorHandler)],
                               debug=True)
